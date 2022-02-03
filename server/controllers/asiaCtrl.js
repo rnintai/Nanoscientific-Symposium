bdb589bce864d51260a42904c7d4b3740f4e673f
park chanhyuk <pgg6713@gmail.com>
2022-02-03 15:22:24 +0900

    wip

--- server/controllers/asiaCtrl.js
+++ server/controllers/asiaCtrl.js
@@ -3,7 +3,6 @@ const path = require("path");
 
 const asiaCtrl = {
     getExhibit: async (req, res) => {
-        console.log(path.join(__dirname,'..','public/asia/exhibit_hall/exhibit.html'))
         res.sendFile(path.join(__dirname,'..','public/asia/exhibit_hall/exhibit.html'))
     }
 }
