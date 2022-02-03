b429b5c9fa68fee8368c3c2e5e8bfbe3c10a4871
park chanhyuk <pgg6713@gmail.com>
2022-02-03 17:18:54 +0900

    [add] routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import './App.css';
 import AsiaExhibit from "./pages/asia/AsiaExhibit";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Landing from "./pages/Landing";
 
 function App() {
 
 
   return (
-    <div>
-      <AsiaExhibit/>
-    </div>
-    // <Routes>
-    //   <Route path='/' element={Landing}/>
-    //   <Route path='/asia/exhibit' element={AsiaExhibit}/>
-    // </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing/>}/>
+        <Route path="/asia/exhibit" element={<AsiaExhibit/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
