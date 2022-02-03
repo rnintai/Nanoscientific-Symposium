ec1a54c0b0e3ff502b1532434b3fc7ff12fa8f83
park chanhyuk <pgg6713@gmail.com>
2022-02-03 10:53:46 +0900

    [init] project

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+const path = require('path')
+const bodyParser = require('body-parser');
+
+const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json())
+app.use(cors())
+
+
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'client','build','index.html'))
+    })
+}
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>{
+    console.log('Sever is running on port ',PORT)
+})
