f973ef7317426cdb77fb33b013af5f7081341494
rnintai <ho46825@gmail.com>
2022-02-08 10:48:59 +0900

    crlf -> lf

--- server/controllers/usersCtrl.js
+++ server/controllers/usersCtrl.js
@@ -1,19 +1,19 @@
-const {asiaPool} = require('../dbConfigPool');
-const hasher = require('wordpress-hash-node');
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
+const { asiaPool } = require("../dbConfigPool");
+const hasher = require("wordpress-hash-node");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
 // const { router } = require('../routes/usersRouter');
 
 const usersCtrl = {
   login: async (req, res, next) => {
-    const connection = await asiaPool.getConnection(async conn => conn);
+    const connection = await asiaPool.getConnection(async (conn) => conn);
 
     const userEmail = req.body.email;
     const userPw = req.body.password;
 
-    let checked = false; 
+    let checked = false;
 
-    try{
+    try {
       const sql = `SELECT email, password FROM user WHERE email='${userEmail}'`;
 
       const result = await connection.query(sql);
@@ -21,65 +21,43 @@ const usersCtrl = {
       let dbPassword = result[0][0].password; // 해쉬화 된 db상의 wordpress 비밀번호
       checked = hasher.CheckPassword(userPw, dbPassword);
       connection.release();
-    } catch(error){
+    } catch (error) {
       await connection.rollback();
       connection.release();
       throw error;
-    } 
+    }
 
-    if(checked){
+    if (checked) {
       // refresh토큰 생성 및 db에 저장
-      let refreshToken = jwt.sign({},
-        process.env.JWT_SECRET,
-        { expiresIn: '14d' });
+      let refreshToken = jwt.sign({}, process.env.JWT_SECRET, {
+        expiresIn: "14d",
+      });
       const insertSql = `UPDATE user SET refresh_token='${refreshToken}' WHERE email='${userEmail}'`;
       try {
         await connection.beginTransaction();
         await connection.query(insertSql);
-        
-        res.status(200).json({result: true});
+
+        res.status(200).json({ result: true });
         await connection.commit();
         connection.release();
 
         // access토큰 생성
-        
+        let accessToken = jwt.sign({ userEmail }, process.env.JWT_SECRET, {
+          expiresIn: "10m",
+        });
+
         // 두 개의 토큰을 client 쿠키에 저장
-      } catch(error) {
+      } catch (error) {
         await connection.rollback();
         connection.release();
         throw error;
       }
-    } else{
+    } else {
       res.status(400).json({
         result: false,
-        message: "user info not match."
+        message: "user info not match.",
       });
     }
-
-    // asiaPool.query(sql, (error, rows) => {
-    //   if (error) throw error;
-    //   let dbPassword = rows[0].password; // 해쉬화 된 db상의 wordpress 비밀번호
-    //   let checked = hasher.CheckPassword(userPw, dbPassword);
-
-    //   if(checked){
-    //     // refresh토큰 생성 및 db에 저장
-    //     let refreshToken = jwt.sign({},
-    //       process.env.JWT_SECRET,
-    //       { expiresIn: '14d' });
-    //     const insertSql = `UPDATE user SET refresh_token='${refreshToken}' WHERE email='${userEmail}'`;
-    //     asiaPool.query(insertSql, (error, rows) => {
-    //       if (error) throw error;
-    //       console.log(rows);
-    //       res.status(200).json({result: true});
-    //       // 
-    //     });
-    //   } else{
-    //     res.status(400).json({
-    //       result: false
-    //     });
-    //   }
-    // });
-
   },
 };
 
