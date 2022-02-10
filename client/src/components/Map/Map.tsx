16ba7507a512dce75641730c655d8208cdb71a24
rnintai <ho46825@gmail.com>
2022-02-10 15:42:46 +0900

    Fix: Map 오류

--- client/src/components/Map/Map.tsx
+++ client/src/components/Map/Map.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { MapContainer } from "./MapStyles";
 
 interface IMap {
-  mapType: google.maps.MapTypeId;
+  // mapType: google.maps.MapTypeId;
   mapTypeControl: boolean;
 }
 
 // 현재 이 컴포넌트를 사용하지 않지만 길찾기 등 동적인 상황이 필요할때 사용합니다
 
-const Map = ({ mapType, mapTypeControl = false }: IMap) => {
+const Map = ({ mapTypeControl = false }: IMap) => {
   return <MapContainer>He;lop</MapContainer>;
 };
 
