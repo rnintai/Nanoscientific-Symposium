294133eeff241e91dfe073dd77a300d2c11ebfb6
rnintai <ho46825@gmail.com>
2022-02-16 09:51:54 +0900

    Init: EU register mkto form

--- client/src/pages/europe/EuropeRegistration/EuropeRegistration.tsx
+++ client/src/pages/europe/EuropeRegistration/EuropeRegistration.tsx
@@ -1,33 +1,56 @@
 import React, { useEffect } from "react";
-// import InnerHTML from "dangerously-set-html-content";
-// import useHTML from "hooks/useHTML";
-// import Loading from "components/Loading/Loading";
+import { Button } from "@mui/material";
 import { RegistrationContainer } from "./EuropeRegistrationStyles";
 
 const EuropeRegistration = () => {
-  // const [HTML, loading] = useHTML("/api/page/europe/registration");
-  // if (loading) {
-  //   return <Loading />;
-  // }
   useEffect(() => {
     const script = document.createElement("script");
     document.body.appendChild(script);
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    window.MktoForms2.loadForm("//pages.parksystems.com", "988-FTP-549", 1065);
+    window.MktoForms2.loadForm(
+      "//pages.parksystems.com",
+      "988-FTP-549",
+      1065,
+      (form: any) => {
+        console.log(form);
+        console.log(form.getFormElem());
+        console.log(form.vals());
+        form.getFormElem()[0].addEventListener("submit", (e: SubmitEvent) => {
+          e.preventDefault();
+        });
+        // form.onSubmit((e: any) => {
+        //   console.log(e);
+        //   e.preventDefault();
+        // });
+        //
+      },
+    );
   }, []);
 
   useEffect(() => {
-    document.getElementById("mktoForm_1071")?.addEventListener("submit", () => {
-      console.log("submitted");
-    });
-    // document.querySelector(".mktoButton")?.addEventListener("click", () => {
-    //   console.log("asd");
-    // });
+    // document
+    //   .getElementById("mktoForm_1065")
+    //   ?.addEventListener("submit", (e: SubmitEvent) => {
+    //     console.log(e);
+    //     e.preventDefault();
+    //     console.log("submitted");
+    //   });
+    document
+      .querySelector(".mktoButton2")
+      ?.addEventListener("click", (e: Event) => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        console.log(window.MktoForms2);
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        console.log(window.MktoForms2.allForms()[0]);
+      });
   }, [document.querySelector(".mktoButton")]);
   return (
     <RegistrationContainer>
       <form id="mktoForm_1065" />
+      <Button className="mktoButton2">check</Button>
     </RegistrationContainer>
   );
 };
