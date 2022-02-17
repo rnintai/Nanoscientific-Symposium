35e99160af6951561d95d32b170143a78d98b536
rnintai <ho46825@gmail.com>
2022-02-17 16:55:43 +0900

    Init: EU 등록 페이지

--- client/src/pages/europe/EuropeRegistration/EuropeRegistration.tsx
+++ client/src/pages/europe/EuropeRegistration/EuropeRegistration.tsx
@@ -1,8 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "@mui/material";
-import { RegistrationContainer } from "./EuropeRegistrationStyles";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import { PayPalButton } from "react-paypal-button-v2";
+
+import { useNavigate } from "react-router";
+import {
+  RegistrationContainer,
+  PayPalContainer,
+} from "./EuropeRegistrationStyles";
 
 const EuropeRegistration = () => {
+  const [checkout, setCheckout] = useState<boolean>(false);
+  const [registerFee, setRegisterFee] = useState<string>("20");
+  const navigate = useNavigate();
+
   useEffect(() => {
     const script = document.createElement("script");
     document.body.appendChild(script);
@@ -13,44 +24,113 @@ const EuropeRegistration = () => {
       "988-FTP-549",
       1065,
       (form: any) => {
-        console.log(form);
-        console.log(form.getFormElem());
-        console.log(form.vals());
-        form.getFormElem()[0].addEventListener("submit", (e: SubmitEvent) => {
-          e.preventDefault();
-        });
-        // form.onSubmit((e: any) => {
-        //   console.log(e);
-        //   e.preventDefault();
-        // });
-        //
+        form.getFormElem()[0][19].remove();
       },
     );
   }, []);
 
   useEffect(() => {
-    // document
-    //   .getElementById("mktoForm_1065")
-    //   ?.addEventListener("submit", (e: SubmitEvent) => {
-    //     console.log(e);
-    //     e.preventDefault();
-    //     console.log("submitted");
-    //   });
     document
       .querySelector(".mktoButton2")
       ?.addEventListener("click", (e: Event) => {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
-        console.log(window.MktoForms2);
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        console.log(window.MktoForms2.allForms()[0]);
+        if (window.MktoForms2.allForms()[0].validate() === true) {
+          setCheckout(true);
+        }
       });
   }, [document.querySelector(".mktoButton")]);
   return (
     <RegistrationContainer>
       <form id="mktoForm_1065" />
-      <Button className="mktoButton2">check</Button>
+      {!checkout && (
+        <Button variant="contained" className="mktoButton2">
+          CHECKOUT
+        </Button>
+      )}
+      {/* {checkout && (
+        <PayPalButton
+        amount="20.00"
+        options={{
+          clientId:
+          "Ab7DDztb5dlZOIvcQER01Iw0RsBanvxQBRNiiL6ORL56nYEZhF0CuSyTNNOj5ZwyTszzAD1ht4P4lGbA",
+        }}
+        />
+      )} */}
+      {checkout && (
+        <div className="paypal-container">
+          <Button
+            onClick={(e: any) => {
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              window.MktoForms2.allForms()[0]
+                .submit()
+                .onSubmit(() => {
+                  // loading
+                  e.preventDefault();
+                  alert("onSubmit");
+                })
+                .onSuccess(() => {
+                  e.preventDefault();
+                  alert("onSuccess");
+                  navigate("/success");
+                  // user db submit
+                  // go to success page
+                });
+            }}
+          >
+            submit
+          </Button>
+          <PayPalScriptProvider
+            options={{
+              "client-id":
+                "Ab7DDztb5dlZOIvcQER01Iw0RsBanvxQBRNiiL6ORL56nYEZhF0CuSyTNNOj5ZwyTszzAD1ht4P4lGbA",
+              currency: "EUR",
+            }}
+          >
+            <PayPalButtons
+              style={{ color: "blue" }}
+              createOrder={(data, actions) => {
+                return actions.order.create({
+                  purchase_units: [
+                    {
+                      amount: {
+                        value: registerFee,
+                      },
+                    },
+                  ],
+                });
+              }}
+              onApprove={async (data, actions) => {
+                const result = actions?.order?.capture().then((details) => {
+                  const transactionId = details.id;
+                  // snackBar?
+                  alert(`Transaction completed. ${transactionId}`);
+                  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                  // @ts-ignore
+                  const formData = window.MktoForms2.allForms()[0].getValues();
+                  console.log(formData);
+                  // marketo submit
+                  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                  // @ts-ignore
+                  window.MktoForms2.allForms()[0]
+                    .submit()
+                    .onSubmit(() => {
+                      // loading
+                      console.log("onSubmit");
+                    })
+                    .onSuccess(() => {
+                      console.log("onSuccess");
+                      // user db submit
+                      // go to success page
+                    });
+                });
+                return result;
+              }}
+            />
+          </PayPalScriptProvider>
+        </div>
+      )}
     </RegistrationContainer>
   );
 };
--- client/src/pages/europe/EuropeRegistration/EuropeRegistrationStyles.tsx
+++ client/src/pages/europe/EuropeRegistration/EuropeRegistrationStyles.tsx
@@ -5,7 +5,22 @@ export const RegistrationContainer = styled.div`
   min-height: calc(100vh - 120px);
   padding: 30px 0;
   margin: 0 auto;
-  form {
+  .mktoForm {
+    margin: 0 auto;
+
+    .mktoField {
+      background-color: white;
+    }
+  }
+  .paypal-container {
+    width: 200px;
     margin: 0 auto;
   }
+
+  .mktoButton2 {
+    left: 50%;
+    transform: translateX(-50%);
+  }
 `;
+
+export const PayPalContainer = styled.div``;
