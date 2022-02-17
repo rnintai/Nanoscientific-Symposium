27f1a3c04663a31c80863c4f55e12d61eab5dc85
park chanhyuk <pgg6713@gmail.com>
2022-02-17 16:55:44 +0900

    fix : hide 상태인 프로그램과 세션 불러오지 않기로 처리

--- server/controllers/koreaCtrl.js
+++ server/controllers/koreaCtrl.js
@@ -12,7 +12,7 @@ const koreaCtrl = {
 
   // 프로그램과 세션 조인시켜서 가져오는것보다 따로가져오는게 프론트에서 랜더링하기 편함
   getPrograms: async (req, res) => {
-    const sql = `SELECT * FROM programs`;
+    const sql = `SELECT * FROM programs where status=1`;
     koreaConnection.query(sql, (error, rows) => {
       if (error) throw error;
       res.send(rows);
@@ -20,7 +20,7 @@ const koreaCtrl = {
   },
 
   getSessions: async (req, res) => {
-    const sql = `SELECT * FROM program_sessions`;
+    const sql = `SELECT * FROM program_sessions where status=1`;
     koreaConnection.query(sql, (error, rows) => {
       if (error) throw error;
       res.send(rows);
