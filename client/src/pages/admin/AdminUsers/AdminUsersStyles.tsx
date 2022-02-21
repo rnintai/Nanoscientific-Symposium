1b2ff4cfb5583d5edff14895c06743e3f3c17574
park chanhyuk <pgg6713@gmail.com>
2022-02-21 13:52:18 +0900

    feat : Users info 테이블로 보여주기

--- client/src/pages/admin/AdminUsers/AdminUsersStyles.tsx
+++ client/src/pages/admin/AdminUsers/AdminUsersStyles.tsx
@@ -1,3 +1,9 @@
 import styled from "styled-components";
 
-export const AdminUsersContainer = styled.div``;
+export const AdminUsersContainer = styled.div`
+  // 페이지 이동시키는거 고정시키도록 이게 없으면 버튼클릭할때맏 사이즈가 왔따리갔다리한다
+
+  .MuiToolbar-root.MuiToolbar-gutters.MuiToolbar-regular.MuiTablePagination-toolbar {
+    width: 50vw;
+  }
+`;
