2dc8c8bc306960fba87887be30677e30204dc6dd
Chanhyuk Park <pgg6713@gmail.com>
2022-02-24 12:04:41 +0900

    add: userRouter swagger

--- server/routes/usersRouter.js
+++ server/routes/usersRouter.js
@@ -2,8 +2,85 @@ const router = require("express").Router();
 const usersCtrl = require("../controllers/usersCtrl");
 const usersMid = require("../middlewares/users");
 
+/**
+ * @swagger
+ *  /api/users/login:
+ *    post:
+ *      tags:
+ *      - User
+ *      description: 로그인
+ *      parameters:
+ *        - name: request
+ *          in: body
+ *          required: true
+ *          schema:
+ *            type: string
+ *            example: {
+ *              "nation": "asia",
+ *              "email": "chanhyuk-tech@kakao.com",
+ *              "password": "parksystems",
+ *            }
+ *      responses:
+ *        '200':
+ *          description: successful operation
+ */
+
+
+
+
+
 router.post("/login", usersCtrl.login);
+
+
+/**
+ * @swagger
+ *  /api/users/logout:
+ *    post:
+ *      tags:
+ *      - User
+ *      description: 로그아웃
+ *      parameters:
+ *        - name: request
+ *          in: body
+ *          required: true
+ *          schema:
+ *            type: string
+ *            example: {
+ *              "nation": "asia",
+ *              "email": "chanhyuk-tech@kakao.com",
+ *              "refreshToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluMSIsImlhdCI6MTY0NTYwMjI3MywiZXhwIjoxNjQ2ODExODczfQ.7PHrIEB4ljAJN6aRHPo3heFMLPAhBQILTa__uW5hHqo",
+ *            }
+ *      responses:
+ *        '200':
+ *          description: successful operation
+ */
+
 router.post("/logout", usersCtrl.logout);
+
+/**
+ * @swagger
+ *  /api/users/check:
+ *    post:
+ *      tags:
+ *      - User
+ *      description: accessToken 유효성 검증
+ *      parameters:
+ *        - name: request
+ *          in: body
+ *          required: true
+ *          schema:
+ *            type: string
+ *            example: {
+ *              "nation": "asia",
+ *              "accessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluMSIsImlhdCI6MTY0NTYwMjI3MywiZXhwIjoxNjQ2ODExODczfQ.7PHrIEB4ljAJN6aRHPo3heFMLPAhBQILTa__uW5hHqo",
+ *            }
+ *      responses:
+ *        '200':
+ *          description: successful operation
+ */
+
+
+
 router.post(
   "/check",
   usersMid.checkToken,
@@ -21,8 +98,85 @@ router.post(
     res.status(200).json(resObj);
   }
 );
+
+
+/**
+ * @swagger
+ *  /api/users/checkemail:
+ *    post:
+ *      tags:
+ *      - User
+ *      description: accessToken 유효성 검증
+ *      parameters:
+ *        - name: request
+ *          in: body
+ *          required: true
+ *          schema:
+ *            type: string
+ *            example: {
+ *              "nation": "asia",
+ *              "email": "chanhyuk-tech@kakao.com",
+ *            }
+ *      responses:
+ *        '200':
+ *          description: successful operation
+ */
+
+
+
 router.post("/checkemail", usersCtrl.checkEmail);
+
+/**
+ * @swagger
+ *  /api/users/passwordset:
+ *    post:
+ *      tags:
+ *      - User
+ *      description: 비밀번호가 설정되지 않은 유저 비밀번호 설정 유도
+ *      parameters:
+ *        - name: request
+ *          in: body
+ *          required: true
+ *          schema:
+ *            type: string
+ *            example: {
+ *              "nation": "asia",
+ *              "email": "chanhyuk-tech@kakao.com",
+ *              "password": "newpassword",
+ *              "firstName": "chanhyuk",
+ *              "lastName": "park",
+ *            }
+ *      responses:
+ *        '200':
+ *          description: successful operation
+ */
+
 router.post("/passwordset", usersCtrl.setPassword);
+
+
+
+/**
+ * @swagger
+ *  /api/users/passwordset/check:
+ *    post:
+ *      tags:
+ *      - User
+ *      description: 유저가 password 설정되어있는지 안되어있는지 판단
+ *      parameters:
+ *        - name: request
+ *          in: body
+ *          required: true
+ *          schema:
+ *            type: string
+ *            example: {
+ *              "nation": "asia",
+ *              "email": "chanhyuk-tech@kakao.com",
+ *            }
+ *      responses:
+ *        '200':
+ *          description: successful operation
+ */
+
 router.post("/passwordset/check", usersCtrl.checkPasswordSet);
 
 module.exports = router;
