96fbc0093a0a45fc0d0a78bc6843d9c81c2aedcd
rnintai <ho46825@gmail.com>
2022-03-03 16:27:16 +0900

    Refactor: isAdmin -> is_published 의미에 맞게 컬럼명 수정.

--- server/controllers/menuCtrl.js
+++ server/controllers/menuCtrl.js
@@ -7,14 +7,14 @@ const menuCtrl = {
 
     const connection = await currentPool.getConnection(async (conn) => conn);
     try {
-      const sql = `SELECT isAdmin FROM menu WHERE path='${path}'`;
+      const sql = `SELECT is_published FROM menu WHERE path='${path}'`;
 
       const row = await connection.query(sql);
 
       if (row[0].length !== 0) {
         res.status(200).json({
           success: true,
-          result: row[0][0].isAdmin,
+          result: row[0][0].is_published,
           msg: "성공",
         });
       } else {
@@ -39,7 +39,7 @@ const menuCtrl = {
 
     const connection = await currentPool.getConnection(async (conn) => conn);
     try {
-      const sql = `UPDATE menu SET isAdmin=${isAdmin} WHERE path='${path}'`;
+      const sql = `UPDATE menu SET is_published=${isAdmin} WHERE path='${path}'`;
 
       await connection.query(sql);
 
