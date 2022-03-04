bd53affc56d822eb6db5210287d9287443ff5636
rnintai <ho46825@gmail.com>
2022-03-04 16:58:58 +0900

    Feat: Timer 컴포넌트 구현

---
+++ client/src/components/Timer/Timer.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { TimerContainer } from "./TimerStyles";
+
+interface TimerProps {
+  second: number;
+  setIsExpired: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Timer = ({ second, setIsExpired }: TimerProps) => {
+  const [timeRemained, setTimeRemained] = useState<number>(second);
+  const minuteRemained = Math.floor(timeRemained / 60);
+  const secondRemained = Math.floor(timeRemained % 60);
+  const [intervalState, setIntervalState] = useState<NodeJS.Timer>();
+
+  useEffect(() => {
+    setIntervalState(
+      setInterval(() => {
+        setTimeRemained((timeRemained) => timeRemained - 1);
+      }, 1000),
+    );
+  }, []);
+
+  useEffect(() => {
+    if (intervalState && timeRemained < 1) {
+      clearInterval(intervalState);
+      setIsExpired(true);
+    }
+  }, [timeRemained]);
+
+  return (
+    <TimerContainer>
+      {minuteRemained}:
+      {secondRemained < 10 ? `0${secondRemained}` : secondRemained}
+    </TimerContainer>
+  );
+};
+
+export default Timer;
---
+++ client/src/components/Timer/TimerStyles.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const TimerContainer = styled.div`
+  display: flex;
+  align-items: center;
+  // color: red;
+  padding: 0 10px;
+`;
