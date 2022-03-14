ead1862064eceaf933115c501330244faa8576e7
rnintai <ho46825@gmail.com>
2022-03-14 18:07:07 +0900

    Feat: duration 계산 util 함수 구현 (date, string)

--- client/src/utils/Date.ts
+++ client/src/utils/Date.ts
@@ -13,11 +13,39 @@ export const dateToLocaleString = (date: string, timeZone: string) => {
   // ex> 2022-01-11 17:30
   return new Date(date).toLocaleString("sv-SE", {
     timeZone,
-    // weekday: "short",
     year: "numeric",
     month: "2-digit",
     day: "2-digit",
     hour: "2-digit",
     minute: "2-digit",
   });
+};
+
+// duration을 계산하여 hh:mm string을 반환하는 메서드
+export const calculateDurationToString = (
+  startTime: string,
+  duration: number,
+  timeZone: string,
+) => {
+  const time = new Date(startTime);
+  const minute = time.getMinutes();
+  time.setMinutes(minute + duration);
+
+  const timeString = time.toLocaleString("sv-SE", {
+    timeZone,
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  return timeString;
+};
+
+export const calculateDurationToDate = (
+  startTime: string,
+  duration: number,
+) => {
+  const time = new Date(startTime);
+  const minute = time.getMinutes();
+  time.setMinutes(minute + duration);
+  return time;
 };
