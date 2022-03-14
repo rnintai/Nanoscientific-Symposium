349b4a6fa5ada68cd5117555f1b5b8dc307eacf4
rnintai <ho46825@gmail.com>
2022-03-14 18:07:26 +0900

    Formatting

--- server/controllers/zoomCtrl.js
+++ server/controllers/zoomCtrl.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 
 const payload = {
   iss: process.env.ZOOM_API_KEY,
-  exp: ((new Date()).getTime() + 5000)
+  exp: new Date().getTime() + 5000,
 };
 
 const token = jwt.sign(payload, process.env.ZOOM_API_SECRET);
@@ -13,21 +13,23 @@ const token = jwt.sign(payload, process.env.ZOOM_API_SECRET);
 const europeCtrl = {
   getWebinars: async (req, res) => {
     try {
-      const response = await axios.get("https://api.zoom.us/v2/users/event@nanoscientific.org/webinars", {
-        headers: {
-          Authorization: `Bearer ${token}`
+      const response = await axios.get(
+        "https://api.zoom.us/v2/users/event@nanoscientific.org/webinars",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
-      });
+      );
       res.status(200).json({
-        result: response.data
+        result: response.data,
       });
     } catch (err) {
       console.log(err);
       res.status(400).json({
-        err
+        err,
       });
     }
-  }
+  },
 };
 module.exports = europeCtrl;
-
