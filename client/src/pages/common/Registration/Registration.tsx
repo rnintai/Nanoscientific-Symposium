d67c376e03c01df3074cd4c47bc765ffd5e10beb
rnintai <ho46825@gmail.com>
2022-03-17 16:06:09 +0900

    Design: register form validation 위치 변경 및 폼 디자인 수정

--- client/src/pages/common/Registration/Registration.tsx
+++ client/src/pages/common/Registration/Registration.tsx
@@ -8,22 +8,27 @@ import useSeoTitle from "hooks/useSeoTitle";
 import { globalData } from "components/NavBar/NavBar";
 import { LoadingButton } from "@mui/lab";
 import { useAuthState, useAuthDispatch } from "context/AuthContext";
+import TopCenterSnackBar from "components/TopCenterSnackBar/TopCenterSnackBar";
 import { RegistrationContainer, MktoFormContainer } from "./RegistrationStyles";
 
 interface RegistrationProps {
   formNo: string;
 }
 
+type TFN = 1 | 0 | -1;
+
 const Registration = ({ formNo }: RegistrationProps) => {
   const [mktoLoading, setMktoLoading] = useState<boolean>(false);
   const [submitBlock, setSubmitBlock] = useState<boolean>(false);
-  const [emailValid, setEmailValid] = useState<boolean>(false);
-  const [emailValidationMsg, setEmailValidationMsg] = useState<string>("");
+  const [emailValid, setEmailValid] = useState<TFN>(-1);
   const navigate = useNavigate();
 
   const nation = usePageViews();
   const authState = useAuthState();
   const dispatch = useAuthDispatch();
+
+  // alert
+  const [emailNotValidAlert, setEmailNotValidAlert] = useState<boolean>(false);
 
   // seo
   const { registration } = globalData.get(nation) as Common.globalDataType;
@@ -52,8 +57,24 @@ const Registration = ({ formNo }: RegistrationProps) => {
       "988-FTP-549",
       formNo,
       (form: any) => {
-        form.getFormElem()[0][14].remove();
+        document.querySelector(".mktoButton[type='submit']")?.remove();
         setMktoLoading(false);
+
+        // validation 끼워넣기
+        const validationDiv = document.createElement("div");
+        validationDiv.className = "validation-msg";
+
+        // 체크박스 layout 변경
+        const check1 = document.querySelector("#LblpsmktOptin")?.parentElement;
+        const check2 = document.querySelector("#LblpsOptin")?.parentElement;
+
+        check1?.classList.add("flex-reverse");
+        check2?.classList.add("flex-reverse");
+        check2?.parentElement?.childNodes[0].remove();
+
+        document
+          .querySelector("#LblEmail")
+          ?.parentElement?.appendChild(validationDiv);
 
         // validation & 중복체크
         document
@@ -64,18 +85,14 @@ const Registration = ({ formNo }: RegistrationProps) => {
               target.value.indexOf("@") === -1 &&
               target.value.indexOf(".") === -1
             ) {
-              setEmailValid(false);
-              setEmailValidationMsg("");
+              setEmailValid(0);
             } else {
               try {
                 const res = await axios.post("/api/users/checkemail", {
                   email: target.value,
                   nation,
                 });
-                setEmailValid(!res.data.result);
-                setEmailValidationMsg(
-                  res.data.result ? "Duplicated email." : "Valid Email !",
-                );
+                setEmailValid(!res.data.result ? 1 : 0);
               } catch (err) {
                 console.log(err);
               }
@@ -84,6 +101,23 @@ const Registration = ({ formNo }: RegistrationProps) => {
       },
     );
   }, []);
+
+  useEffect(() => {
+    if (document.querySelector(".validation-msg") !== null) {
+      const validationDOM = document.querySelector(
+        ".validation-msg",
+      ) as HTMLParagraphElement;
+      if (emailValid === 1) {
+        validationDOM.classList.add("valid");
+        validationDOM.classList.remove("invalid");
+        validationDOM.innerText = "Valid Email!";
+      } else if (emailValid === 0) {
+        validationDOM.classList.add("invalid");
+        validationDOM.classList.remove("valid");
+        validationDOM.innerText = "Invalid or duplicate email.";
+      }
+    }
+  }, [emailValid]);
 
   const submitHandler = async () => {
     const formData =
@@ -95,18 +129,15 @@ const Registration = ({ formNo }: RegistrationProps) => {
     try {
       // user db submit
       const regResponse = await axios.post("/api/users/register", {
-        email: formData.Email,
-        title: formData.Title,
-        university: formData.Company,
-        institute: formData.Department,
-        street: formData.City,
-        zipCode: formData.PostalCode,
-        city: formData.State,
-        researchField: formData.psResearchTopic,
-        afmTool: formData.psExistingAFMBrand,
-        lastName: formData.LastName,
+        title: formData.Salutation,
         firstName: formData.FirstName,
-        psOptIn: formData.psOptIn === "yes" ? 1 : 0,
+        lastName: formData.LastName,
+        email: formData.Email,
+        phone: formData.Phone,
+        institute: formData.Company,
+        department: formData.Department,
+        country: formData.Country,
+        state: formData.State,
         nation,
       });
 
@@ -144,13 +175,6 @@ const Registration = ({ formNo }: RegistrationProps) => {
     <>
       {mktoLoading && <Loading />}
       <RegistrationContainer>
-        <Box
-          className={
-            emailValid ? "validation-msg valid" : "validation-msg invalid"
-          }
-        >
-          &nbsp;{emailValidationMsg}
-        </Box>
         <MktoFormContainer>
           <form id={`mktoForm_${formNo}`} />
           {!mktoLoading && (
@@ -159,17 +183,17 @@ const Registration = ({ formNo }: RegistrationProps) => {
               className="mktoButton2"
               loading={submitBlock}
               onClick={() => {
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-ignore
-                if (window.MktoForms2.allForms()[0]?.validate() && emailValid) {
-                  submitHandler();
-                } else {
+                if (
                   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
                   // @ts-ignore
-                  // window.MktoForms2.allForms()[0].getFormElem()[0][0].scrollIntoView({
-                  //   behavior: "smooth",
-                  // });
+                  !window.MktoForms2.allForms()[0]?.validate()
+                ) {
+                  // 마케토 validator가 알려줌
+                } else if (emailValid !== 1) {
+                  setEmailNotValidAlert(true);
                   window.scrollTo({ top: 0, behavior: "smooth" });
+                } else {
+                  submitHandler();
                 }
               }}
             >
@@ -177,6 +201,13 @@ const Registration = ({ formNo }: RegistrationProps) => {
             </LoadingButton>
           )}
         </MktoFormContainer>
+        <TopCenterSnackBar
+          value={emailNotValidAlert}
+          setValue={setEmailNotValidAlert}
+          severity="warning"
+          content="Email already exists or not valid."
+          variant="filled"
+        />
       </RegistrationContainer>
     </>
   );
