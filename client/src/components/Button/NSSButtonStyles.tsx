57a5d7589c709d2763477cbb986ba02a9b505761
rnintai <ho46825@gmail.com>
2022-03-18 23:59:37 +0900

    Feat: Button 컴포넌트 구현

---
+++ client/src/components/Button/NSSButtonStyles.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import { useTheme } from "@mui/material";
+
+export const NSSButtonContainer = styled.button`
+  transition: background-color 0.2s ease-in-out;
+  padding: 15px 10px;
+  &.primary {
+    color: ${() => {
+      const theme = useTheme();
+      return theme.palette.text.primary;
+    }};
+    &:hover {
+      background-color: rgba(33, 173, 229, 0.04);
+    }
+    &:active {
+      background-color: rgba(255, 255, 255, 0.1);
+    }
+  }
+  &.gradient {
+    background: ${() => {
+      const theme = useTheme();
+      return theme.palette.primary.gradation;
+    }};
+    border: 2px solid #fff;
+    border-radius: 25px;
+    padding: 2px 12px;
+  }
+  display: flex;
+  align-items: center;
+`;
