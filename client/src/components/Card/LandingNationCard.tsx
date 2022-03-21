ea31528c17461c7e5ff0a50b0a7d48f3d73e75e0
rnintai <ho46825@gmail.com>
2022-03-21 14:23:59 +0900

    Feat: [메인 랜딩페이지] 반응형 css 및 국가별 버튼 링크 추가

--- client/src/components/Card/LandingNationCard.tsx
+++ client/src/components/Card/LandingNationCard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/require-default-props */
 import React from "react";
 import { Stack, Typography, useTheme } from "@mui/material";
 import { LandingNationCardContainer } from "./LandingNationCardStyles";
@@ -6,17 +7,30 @@ interface LandingNationCardProps {
   name: string;
   date: string;
   image: string;
+  path: string;
+  disabled?: boolean;
 }
 
-const LandingNationCard = ({ name, date, image }: LandingNationCardProps) => {
+const LandingNationCard = ({
+  name,
+  date,
+  image,
+  path,
+  disabled,
+}: LandingNationCardProps) => {
   const theme = useTheme();
 
   return (
-    <LandingNationCardContainer>
+    <LandingNationCardContainer
+      className={`${disabled ? "disabled" : ""} hover-zoom`}
+      onClick={() => {
+        if (!disabled) {
+          window.open(path, "_blank");
+        }
+      }}
+    >
       <Stack className="card-wrap">
-        <img src={image} alt={`${name}-img`} style={{ height: "180px" }} />
-        {/* <Stack >
-        </Stack> */}
+        <img src={image} alt={`${name}-img`} />
         <Typography
           variant="body2"
           sx={{
@@ -24,7 +38,7 @@ const LandingNationCard = ({ name, date, image }: LandingNationCardProps) => {
             color: "#fff",
             mx: "10px",
             mt: 2,
-            borderRadius: "10px 10px 0 0",
+            borderRadius: "10px",
           }}
         >
           NS {name}
--- client/src/components/Card/LandingNationCardStyles.tsx
+++ client/src/components/Card/LandingNationCardStyles.tsx
@@ -1,10 +1,29 @@
 import styled from "styled-components";
 
-export const LandingNationCardContainer = styled.div`
-  width: 20%;
+export const LandingNationCardContainer = styled.button`
+  &.disabled {
+    pointer-events: none;
+    opacity: 0.6;
+  }
   text-align: center;
 
-  &:not(:nth-child(3n)) {
-    margin-right: 120px;
+  margin-bottom: 30px;
+  @media screen and (max-width: 1920px) {
+    width: 20%;
+    &:not(:nth-child(2n)) {
+      margin-right: 0;
+    }
+    &:not(:nth-child(3n)) {
+      margin-right: 120px;
+    }
+  }
+  @media screen and (max-width: 1024px) {
+    width: 40%;
+    &:not(:nth-child(3n)) {
+      margin-right: 0;
+    }
+    &:not(:nth-child(2n)) {
+      margin-right: 40px;
+    }
   }
 `;
