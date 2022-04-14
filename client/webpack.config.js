f0cb3883fc0bcb7ddaa29b4858c64b9724910378
rnintai <ho46825@gmail.com>
2022-04-14 18:09:54 +0900

    chore: 불필요한 process package 제거.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,11 +1,20 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
 const webpack = require("webpack");
+
+// .env
 const dotenv = require("dotenv");
 
+// 웹팩에서 html을 파싱하기 위함.
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// tsconfig에서 baseUrl을 받아오기 위함
 const TsConfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
+// gzip
 const CompressionPlugin = require("compression-webpack-plugin");
+
+// 번들 점유 용량 확인
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
 // Typescript(타입스크립트)를 빌드할 때 성능을 향상시키기 위한 플러그인를 불러오기
@@ -52,23 +61,27 @@ module.exports = {
         ],
         exclude: /node_modules/,
       },
+      // css, scss loader 설정
       {
         test: /\.(s*)css$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
+  // proxy 설정
   devServer: {
     historyApiFallback: true,
     proxy: {
       "/api": "http://localhost:5000",
     },
   },
+  // uglify
   optimization: {
     minimize: true,
     minimizer: [new TerserPlugin()],
   },
   plugins: [
+    // 기본 html 위치 설정.
     new HtmlWebpackPlugin({
       template: "./public/index.html",
       filename: "index.html",
@@ -76,12 +89,12 @@ module.exports = {
     // Typescript(타입스크립트)의 컴파일 속도 향상을 위한 플러그인을 설정
     new ForkTsCheckerWebpackPlugin(),
     // new BundleAnalyzerPlugin(),
+    // gzip (압축)
     new CompressionPlugin({
       algorithm: "gzip",
     }),
-    new webpack.ProvidePlugin({
-      process: "process/browser",
-    }),
+
+    // env 변수 사용을 위한 플러그인
     new webpack.DefinePlugin({
       "process.env.REACT_APP_S3_ACCESS_KEY": JSON.stringify(
         process.env.REACT_APP_S3_ACCESS_KEY,
