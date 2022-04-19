4352ce23ca0b8cadcd6cf830f2f6709cc89f7218
rnintai <ho46825@gmail.com>
2022-04-19 17:54:40 +0900

    chore: webpack chunkhash 추가

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist/"),
     publicPath: "/",
-    filename: "build.js",
+    filename: "build.[chunkhash].js",
   },
   resolve: {
     plugins: [new TsConfigPathsPlugin()],
