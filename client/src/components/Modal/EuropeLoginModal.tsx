bead0fc528f1b750b1a1d621ab22888bc59451fb
rnintai <ho46825@gmail.com>
2022-05-12 13:04:43 +0900

    Fix: 경고 주석처리

--- client/src/components/Modal/EuropeLoginModal.tsx
+++ client/src/components/Modal/EuropeLoginModal.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useEffect, useState, useRef } from "react";
 import Link from "components/Link/LinkWithSearch";
 import { useNavigate } from "hooks/useNavigateWithSearch";
