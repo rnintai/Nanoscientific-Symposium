ef890ba27f9989273a0061882bcf124e6832184c
rnintai <ho46825@gmail.com>
2022-05-18 16:09:18 +0900

    Fix: utm코드 이외의 쿼리스트링 제거.

--- client/src/components/Link/LinkWithSearch.tsx
+++ client/src/components/Link/LinkWithSearch.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/require-default-props */
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useSearchParams } from "react-router-dom";
 
 interface linkProps extends React.ComponentPropsWithoutRef<"a"> {
   to: string;
@@ -10,8 +10,33 @@ interface linkProps extends React.ComponentPropsWithoutRef<"a"> {
 const LinkWithSearch = (props: linkProps) => {
   const { to, children, ...rest } = props;
   const { search } = useLocation();
+  const utmKeyList = [
+    ...search
+      .replace("?", "")
+      .split("&")
+      .filter((e) => e.indexOf("utm") !== -1)
+      .map((e) => e.split("=")[0]),
+  ];
+  const utmValueList = [
+    ...search
+      .replace("?", "")
+      .split("&")
+      .filter((e) => e.indexOf("utm") !== -1)
+      .map((e) => e.split("=")[1]),
+  ];
+
+  let newSearch = "?";
+  utmKeyList.map((k, i) => {
+    if (i < utmKeyList.length - 1) newSearch += `${k}=${utmValueList[i]}&`;
+    else {
+      newSearch += `${k}=${utmValueList[i]}`;
+    }
+    return null;
+  });
+
+  // console.log(searchParams);
   return (
-    <Link to={to + search} {...rest}>
+    <Link to={to + newSearch} {...rest}>
       {children}
     </Link>
   );
