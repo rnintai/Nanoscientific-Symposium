202b075b7258777637ec265e50c66c571f2db110
rnintai <ho46825@gmail.com>
2022-08-29 14:27:05 +0900

    Feat: sitemap

---
+++ client/src/sitemap/SiteMapRoutes.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Switch, Route, Routes } from "react-router";
+
+const RouteArr = (pathname) => {
+  return [
+    {
+      path: `${pathname}/`,
+    },
+    {
+      path: `${pathname}/registration`,
+    },
+    {
+      path: `${pathname}/program`,
+    },
+    {
+      path: `${pathname}/speakers`,
+    },
+    {
+      path: `${pathname}/lecture-hall`,
+      isPrivate: true,
+    },
+    {
+      path: `${pathname}/exhibit/parksystems`,
+    },
+    {
+      path: `${pathname}/exhibit/nanoscientific`,
+    },
+    {
+      path: `${pathname}/user/reset-password`,
+    },
+    {
+      path: `${pathname}/user/forgot-password`,
+    },
+    {
+      path: `${pathname}/announcement`,
+    },
+    {
+      path: `${pathname}/abstract`,
+    },
+    {
+      path: `${pathname}/poster-hall`,
+      isPrivate: true,
+    },
+  ];
+};
+
+const SiteMapRoutes = (
+  <Route>
+    {RouteArr("americas").map((route) => {
+      return <Route key={route.path} path={route.path} />;
+    })}
+    {RouteArr("eu").map((route) => {
+      return <Route key={route.path} path={route.path} />;
+    })}
+    {RouteArr("asia").map((route) => {
+      return <Route key={route.path} path={route.path} />;
+    })}
+    {RouteArr("kr").map((route) => {
+      return <Route key={route.path} path={route.path} />;
+    })}
+    {RouteArr("jp").map((route) => {
+      return <Route key={route.path} path={route.path} />;
+    })}
+  </Route>
+);
+
+export default SiteMapRoutes;
